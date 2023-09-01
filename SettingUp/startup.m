8f8a414629d91afffd72958cb2e938ada3e659d8
thomassauter <73580875+thomassauter@users.noreply.github.com>
2023-09-01 09:20:01 +0200

    Update startup.m

--- SettingUp/startup.m
+++ SettingUp/startup.m
@@ -2,18 +2,7 @@ disp(' ')
 disp('Hello Thomas!') 
 
 solverOK=changeCobraSolver('ibm_cplex','all')
-% solverOK = changeCobraSolver("ibm_cplex")
 
-% % % currentPath=pwd;
-% % % cd('C:\Users\thomas.sauter\Documents\MATLAB\IQMtools_V1222')
-% % % installIQMtools
-% % % cd(currentPath)
-% % % disp(' ')
-% % % disp('... IQM toolbox added ...')
-% % % disp(' ')
-
-% solverOK=changeCobraSolver('ibm_cplex','all');
-% addpath('C:\Users\thomas.sauter\Documents\MATLAB\rFASTCORMICS_0320')
 addpath(genpath('C:\Users\thomas.sauter\Documents\MATLAB\rFASTCORMICS-master_010923'))
 clear x k currentPath
 disp(' ')
@@ -29,7 +18,4 @@ disp(' ')
 disp(' ') 
 disp('... welcome back ...') 
 disp(' ') 
-
-  
-
  
