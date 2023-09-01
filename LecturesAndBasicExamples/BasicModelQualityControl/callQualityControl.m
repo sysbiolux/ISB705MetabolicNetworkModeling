525454f93e25a0b38f6afd7b01909685aa60c8b1
thomassauter <73580875+thomassauter@users.noreply.github.com>
2023-09-01 11:48:26 +0200

    Add files via upload

--- LecturesAndBasicExamples/BasicModelQualityControl/callQualityControl.m
+++ LecturesAndBasicExamples/BasicModelQualityControl/callQualityControl.m
@@ -1,12 +1,15 @@
-%driver script
-clear 
-addpath(genpath('C:\Program Files\IBM\ILOG\\CPLEX_Studio1210\cplex\matlab\x64_win64'))
-changeCobraSolver('ibm_cplex')
-%model=readCbModel('C:\Users\maria.pacheco\OneDrive - University of Luxembourg\Documents\GitHub\ISB705MetabolicNetworkModelling\Model\Blautia_hydrogenotrophica_DSM_10507.xml');
+%% driver script
+clearvars -except solverOK, clc, close all
+% initialize cplex if needed: 
+% addpath(genpath('C:\Program Files\IBM\ILOG\\CPLEX_Studio1210\cplex\matlab\x64_win64'))
+% changeCobraSolver('ibm_cplex')
 
-model=readCbModel('C:\Users\maria.pacheco\Documents\GitHub\basicAnalysis\Model\Blautia_hydrogenotrophica_DSM_10507.xml');
+%% read model 1 (adapt file path if necessary)
+model=readCbModel('..\..\Model\Blautia_hydrogenotrophica_DSM_10507.xml')
+% fastbox toolbox needed here (add to path):
 [TableNumerics, Required4biomass, Results, minimalMedia, model, consistentModel]=InitialQualityControl(model, 'EX_biomass(e)');
 
-
-load('C:\Users\maria.pacheco\Documents\GitHub\basicAnalysis\Model\brain_model.mat')
-[TableNumerics2, Required4biomass2, Results2, minimalMedia2, model2]=InitialQualityControl(brain_model, 'biomass_maintenance');
+%% read model 2 (adapt file path if necessary)
+load('..\..\Model\brain_model.mat')
+% fastbox toolbox needed here (add to path):
+[TableNumerics2, Required4biomass2, Results2, minimalMedia2, model2, consistentModel2]=InitialQualityControl(brain_model, 'biomass_maintenance');
