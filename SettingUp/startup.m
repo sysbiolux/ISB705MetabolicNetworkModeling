439eb8fd50932748f7c40e5d9832483672034556
thomassauter <73580875+thomassauter@users.noreply.github.com>
2023-09-01 09:19:12 +0200

    Add files via upload

---
+++ SettingUp/startup.m
@@ -0,0 +1,35 @@
+disp(' ') 
+disp('Hello Thomas!') 
+
+solverOK=changeCobraSolver('ibm_cplex','all')
+% solverOK = changeCobraSolver("ibm_cplex")
+
+% % % currentPath=pwd;
+% % % cd('C:\Users\thomas.sauter\Documents\MATLAB\IQMtools_V1222')
+% % % installIQMtools
+% % % cd(currentPath)
+% % % disp(' ')
+% % % disp('... IQM toolbox added ...')
+% % % disp(' ')
+
+% solverOK=changeCobraSolver('ibm_cplex','all');
+% addpath('C:\Users\thomas.sauter\Documents\MATLAB\rFASTCORMICS_0320')
+addpath(genpath('C:\Users\thomas.sauter\Documents\MATLAB\rFASTCORMICS-master_010923'))
+clear x k currentPath
+disp(' ')
+disp('... fastcore / rFASTCORMICS added ...')
+disp(' ')
+
+addpath(genpath('C:\Users\thomas.sauter\OneDrive - University of Luxembourg\uni.lu\MISB\ISB705\SelfStudyCourse_2023\fastbox-main'))
+clear x k currentPath
+disp(' ')
+disp('... fastbox added ...')
+disp(' ')
+
+disp(' ') 
+disp('... welcome back ...') 
+disp(' ') 
+
+  
+
+ 
